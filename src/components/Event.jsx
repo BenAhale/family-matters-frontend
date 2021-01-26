cdda59fe3faeadbefb3c323298e0d1de1a4abfea
Amelia Davis <ameliadavis271@gmail.com>
2021-01-25 22:08:44 -0800

    Calendar logs events day for day selected (react only/dummy DB)

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,6 +2,27 @@ import React from "react";
 import moment from "moment";
 
 export function Event(props) {
-  const eventDate = moment(props.date).format("dddd Do MMMM YYYY");
-  return <h1>Event on {eventDate} </h1>;
+  const eventDate = moment(props.date).format("LL");
+  console.log(props.events);
+  console.log(props.date);
+
+  const todaysEvents = props.events.filter((event) => event.date === eventDate);
+  console.log(todaysEvents);
+
+  return (
+    <div className="event">
+      <h1>Event on {eventDate} </h1>
+      {todaysEvents.map((event, index) => {
+        // if (event.date === String(eventDate)) {
+        return (
+          <div key={event.id}>
+            <h2>{event.name}</h2>
+            <p>{event.time}</p>
+            <p>{event.date}</p>
+          </div>
+        );
+        // }
+      })}
+    </div>
+  );
 }
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { Event } from "./Event";
 
 export function Events() {
   const [date, setDate] = useState(new Date());
-  console.log(date.toLocaleDateString());
+  const [events, setEvents] = useState([]);
+  //   console.log(date.toLocaleDateString());
+
+  useEffect(() => {
+    // For privacy can hide URL in .env.development file where it is declared
+    fetch(`${process.env.REACT_APP_DUMMY_API}`)
+      .then((response) => response.json())
+      .then((body) => setEvents(body));
+  }, []);
+  //   console.log(events);
 
   return (
     <div>
       <h1>Events Page</h1>
       <Calendar onChange={setDate} date={date} />
-      <Event date={date} />
+      <Event date={date} events={events} />
     </div>
   );
 }
