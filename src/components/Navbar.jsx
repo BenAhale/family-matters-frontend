9b675e2860ca1800a7b3ec0324b92189c5e5454c
Ben Ahale <69181751+BenAhale@users.noreply.github.com>
2021-02-05 22:10:28 +1100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,6 @@ export function Navbar() {
 
   function signOut() {
     localStorage.removeItem("token")
-<<<<<<< HEAD
-=======
-    history.push("/")
->>>>>>> c796df86b43c9bba6c67407ba4614232e9d913e0
   }
 
   return (
