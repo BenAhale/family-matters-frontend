319a9419cdcda7ff4a45c71e54902c4cb62f2449
Amelia Davis <ameliadavis271@gmail.com>
2021-02-06 19:44:22 -0800

    Cypress added with first test included

---
+++ cypress/e2e/redirtectToSignIn.spec.js
@@ -0,0 +1,15 @@
+import { cyan } from "@material-ui/core/colors";
+
+describe("Redirects from event page due to login required", () => {
+  it("should have url /events", () => {
+    // test code, will naturally redirect to sign-in
+    cy.visit("/events");
+    cy.url().should("include", "/events");
+  });
+  it("should be redirected to /sign-in", () => {
+    cy.url().should("include", "/sign-in");
+  });
+  it("should have a h1 of 'Login' due to redirecting", () => {
+    cy.get("h1").should("have.text", "Login");
+  });
+});
