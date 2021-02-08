4c626fc7f3a318946b27991551daeeeaa00a354e
Ben Ahale <ahaleben@gmail.com>
2021-02-08 17:54:21 +1100

    Added component to handle image uploads

--- src/components/ImageForm.jsx
+++ src/components/ImageForm.jsx
@@ -1,21 +1,90 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-// export function ImageForm() {
+export function ImageForm() {
 
-//   const state = { selectedFile: null }
+  const [picture, setPicture] = useState("Original Value");
+  let url = ""
+  let history = useHistory();
+  const [description, setDescription] = useState("")
 
-//   const fileChangedHandler = event => {
-//     this.setState({ selectedFile: event.target.files[0] })
-//   }
+  const onChangeHandler = (e) => {
+    setPicture(e.target.files[0])
+  }
 
-//   const uploadHandler = () => {
-//     console.log(state.selectedFile)
-//   }
+  const descriptionHandler = (e) => {
+    setDescription(e.target.value)
+  }
 
-//   return (
-//     <div>
-//       <input type="file" onChange={this.fileChangedHandler} />
-//       <button onClick={this.uploadHandler}>Upload!</button>
-//     </div>
-//   )
-// }
+  async function onFormSubmit(e) {
+    e.preventDefault();
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/memories/url`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({
+        image: {
+          name: picture.name
+        },
+      }),
+    })
+    .then((response) => response.json())
+    .then((body) => {
+      url = body.url
+    })
+    .then(uploadFile)
+  }
+
+  async function uploadFile() {
+    await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": ""
+      },
+      body: picture
+    })
+    .then(response => console.log(response))
+    .then(updateDatabase)
+    .catch(error => console.log(error))
+  }
+
+  async function updateDatabase() {
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/memories/new`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      },
+      body: JSON.stringify({
+        image: {
+          name: picture.name,
+          description: description,
+        }
+      })
+    })
+    .then(response => response.json())
+    .then(body => console.log(body))
+    .then(history.push("/memories"))
+    .catch(error => console.log(error))
+  }
+
+  // async function getImage() {
+  //   await fetch(`${process.env.REACT_APP_BACKEND_URL}/memories/geturl`, {
+  //     method: "GET"
+  //   })
+  //   .then(response => response.json())
+  //   .then((body) => {
+  //     setNewUrl(body.url)
+  //   })
+  // }
+
+  return (
+    <div>
+      <input type="file" onChange={onChangeHandler} />
+      <input type="text" onChange={descriptionHandler} />
+      <button onClick={onFormSubmit}>Upload!</button>
+    </div>
+  )
+}
