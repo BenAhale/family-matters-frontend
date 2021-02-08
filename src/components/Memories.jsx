abf51e28789a84cd307d851bdb837a76c66c35d0
Ben Ahale <ahaleben@gmail.com>
2021-02-08 17:54:32 +1100

    Memories index page

--- src/components/Memories.jsx
+++ src/components/Memories.jsx
@@ -1,24 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { Albums } from "./Albums";
-import { NewAlbum } from "./NewAlbum";
+import { useHistory } from 'react-router-dom'
 
 export function Memories() {
-  const [albums, setAlbums] = useState([]);
-  //   console.log(date.toLocaleDateString());
+  const [memories, setMemories] = useState(null);
+  let history = useHistory()
 
   useEffect(() => {
-    // For privacy can hide URL in .env.development file where it is declared
-    fetch(`${process.env.REACT_APP_ALBUMS_API}`)
-      .then((response) => response.json())
-      .then((body) => setAlbums(body));
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/memories/all`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    .then(response => response.json())
+    .then(body => setMemories(body))
   }, []);
-  //   console.log(events);
+
+  async function deleteMemory(e, id) {
+    e.preventDefault()
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/memories/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    });
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/memories/all`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    .then(response => response.json())
+    .then(body => setMemories(body))
+  }
+
+  function newImage() {
+    history.push("/memories/new")
+  }
 
   return (
     <div>
+      <button onClick={newImage}>New Image</button>
       <h1>Memories Page</h1>
-      <NewAlbum />
-      <Albums albums={albums} />
+      <div>
+        {!(memories === null) && memories.map((memory, index) => { 
+          return ([
+          <img alt="memory" src={memory.url} key={index}/>,
+          <p>{memory.description}</p>,
+          <button onClick={(e) => deleteMemory(e, memory.id)}>Delete</button>
+          ])
+        } ) }
+        <p></p>
+      </div>
+
+      {/* <img src="https://family-matters.s3.ap-southeast-2.amazonaws.com/98f0ea2b/danandrews.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAZPLUNIQPS2V3SGR3%2F20210208%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Date=20210208T040042Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=b3e5ae70cbc335c682d1c96945b3bc1e5b8566c84a4b23224bbea9e16c3661fd"/> */}
+      {/* <NewAlbum /> */}
+      {/* <Albums albums={albums} /> */}
     </div>
   );
 }
