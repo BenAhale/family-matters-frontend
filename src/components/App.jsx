0e950f4054a810e97c77300c92ad5a95ccb62a3e
Ben Ahale <ahaleben@gmail.com>
2021-02-08 17:53:40 +1100

    Removed unneeded imports. Added protected routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Home } from "./Home";
 import { Events } from "./Events";
 import { Memories } from "./Memories";
-import { Groceries } from "./Groceries";
-import { Chores } from "./Chores";
 import "../styles/App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ProtectedRoute } from './ProtectedRoute'
@@ -22,14 +20,12 @@ function App() {
         <Switch>
           {/* defining routes with paths */}
           {/* first with exact prevents double rendering */}
-          <Route exact path="/" component={Home} />
+          <ProtectedRoute exact path="/" component={Home} />
           <Route path="/sign-in" component={SignIn} />
           <Route path="/sign-up" component={SignUp} />
-          <Route path="/memories" component={Memories} />
+          <ProtectedRoute exact path="/memories" component={Memories} />
           <ProtectedRoute path="/events" component={Events} />
-          <Route path="/groceries" component={Groceries} />
-          <Route path="/chores" component={Chores} />
-          <Route path="/images" component={ImageForm} />
+          <ProtectedRoute exact path="/memories/new" component={ImageForm} />
         </Switch>
       </div>
     </BrowserRouter>
